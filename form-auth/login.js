d167863ef16261cb83f0ece02d72ce6e45988bf9
dhp123456-del <dinhhoangphuong11042005@gmail.com>
2025-06-28 13:32:14 +0700

    init

---
+++ form-auth/login.js
@@ -0,0 +1,31 @@
+document.getElementById("auth-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const email = document.getElementById("email").value.trim();
+  const password = document.getElementById("password").value;
+
+  const user = users.find(u => u.email === email && u.password === password);
+
+  if (user) {
+    showLoading();
+
+    setTimeout(() => {
+      localStorage.setItem("loggedInUser", JSON.stringify(user));
+      localStorage.setItem("isLoggedIn", "true");
+      window.location.href = "../index.html";
+    }, 3000);
+  } else {
+    alert("Sai tài khoản hoặc mật khẩu!");
+    window.location.href = "login.html";
+  }
+});
+
+function showLoading() {
+  const formBox = document.getElementById("form-box");
+  formBox.innerHTML = `
+    <div class="loader-container">
+      <div class="loader"></div>
+      <p>Đang xử lý, vui lòng đợi...</p>
+    </div>
+  `;
+}
---
+++ form-auth/script.js
@@ -0,0 +1,16 @@
+let isLogin = true;
+
+function toggleForm() {
+  isLogin = !isLogin;
+
+  const title = document.getElementById("form-title");
+  const toggleText = document.getElementById("toggle-text");
+
+  if (isLogin) {
+    title.textContent = "Đăng nhập";
+    toggleText.innerHTML = `Chưa có tài khoản? <a href="#" onclick="toggleForm()">Đăng ký</a>`;
+  } else {
+    title.textContent = "Đăng ký";
+    toggleText.innerHTML = `Đã có tài khoản? <a href="#" onclick="toggleForm()">Đăng nhập</a>`;
+  }
+}
